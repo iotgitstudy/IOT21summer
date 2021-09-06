88ac0b756490f7da5c8e9dee34fe81f809b39e5a
namjiseong <skawltjd2000@naver.com>
2021-09-06 22:43:16 +0900

    21. 레이아웃 후반전 made by 지성

--- python/GUI_project/1_create_layout.py
+++ python/GUI_project/1_create_layout.py
@@ -1,4 +1,5 @@
 #import
+import tkinter.ttk as ttk
 from tkinter import *
 #기본 head
 root = Tk()
@@ -7,7 +8,7 @@ root.title("GUI")
 
 # 파일 프레임
 file_frame = Frame(root)
-file_frame.pack()
+file_frame.pack(fill="x")
 
 btn_add_file = Button(file_frame, text="파일추가", padx=5, pady=5, width=12)
 btn_add_file.pack(side="left")
@@ -30,7 +31,7 @@ scrollbar.config(command=list_file.yview)
 
 #저장 경로 프레임
 path_frame = LabelFrame(root, text="저장경로")
-path_frame.pack()
+path_frame.pack(fill="x")
 
 txt_dest_path = Entry(path_frame)
 txt_dest_path.pack(side="left", fill="x", expand=True, ipady=4) # 높이 확장
@@ -38,8 +39,59 @@ txt_dest_path.pack(side="left", fill="x", expand=True, ipady=4) # 높이 확장
 btn_dest_path = Button(path_frame, text="찾아보기", width=10)
 btn_dest_path.pack(side="right")
 
+# 옵션 프레임
+frame_option = LabelFrame(root, text="옵션")
+frame_option.pack()
+
+# 1. 가로 넓이 옵션
+# 가로 넓이 레이블
+lbl_width = Label(frame_option, text="가로넓이", width=8)
+lbl_width.pack(side="left")
+
+# 가로 넓이 콤보
+opt_width = ["원본유지", "1024", "800", "640"]
+cmb_width = ttk.Combobox(frame_option, state="readonly", values=opt_width, width=10)
+cmb_width.current(0)
+cmb_width.pack(side="left")
+# 2. 간격 옵션
+#간격 옵션 레이블
+lbl_space = Label(frame_option, text="간격", width=8)
+lbl_space.pack(side="left")
+#간격 옵션 콤보
+opt_space = ["없음", "좁게", "보통", "넓게"]
+cmb_space = ttk.Combobox(frame_option, state="readonly", values=opt_space, width=10)
+cmb_space.current(0)
+cmb_space.pack(side="left")
+
+# 3. 파일 포맷 옵션
+# 파일 포맷 옵션 레이블
+lbl_format = Label(frame_option, text="간격", width=8)
+lbl_format.pack(side="left")
+# 파일 포맷 옵션 콤보
+opt_format = ["PNG", "JPG", "BMP"]
+cmb_format = ttk.Combobox(frame_option, state="readonly", values=opt_format, width=10)
+cmb_format.current(0)
+cmb_format.pack(side="left")
+
+# 진행 상황 Progress Bar
+frame_progress = LabelFrame(root, text="진행상황")
+frame_progress.pack(fill="x")
 
 
+p_var = DoubleVar()
+progress_bar = ttk.Progressbar(frame_progress, maximum=100, variable=p_var)
+progress_bar.pack(fill="x")
+
+
+# 실행 프레임
+frame_run = Frame(root)
+frame_run.pack(fill="x")
+
+btn_start = Button(frame_run, padx=5, pady=5, text="시작", width=12)
+btn_start.pack(side="right")
+
+btn_close = Button(frame_run, padx=5, pady=5, text="닫기", width=12)
+btn_close.pack(side="right")
 
 #화면 설정
 root.resizable(False, False)
