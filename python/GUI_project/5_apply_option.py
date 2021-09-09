c9d1a00d75bcedc2204f83d8a787cb4eae0e1019
namjiseong <skawltjd2000@naver.com>
2021-09-09 21:28:08 +0900

    30. 옵션 후반전 made by 지성

--- python/GUI_project/5_apply_option.py
+++ python/GUI_project/5_apply_option.py
@@ -85,21 +85,29 @@ def merge_image():
     max_width, total_height = max(widths), sum(heights)
 
     # 스케치북 준비
+    #이미지 간격 옵션 적용 
+    if img_space > 0:
+        total_height += (img_space * (len(images) - 1))
     result_img = Image.new("RGB", (max_width, total_height), (255,255,255)) # 배경 색 
     y_offset = 0 # y 위치
 
-    # for img in images:
-    #     result_img.paste(img, (0, y_offset))
-    #     y_offset += img.size[1] # height 값 만큼 더해줌
+   
+
     for idx, img in enumerate(images):
+        # width 가 원본 유지가 아닐 때에는 이미지 크기 조정
+        if img_width > -1:
+            img = img.resize(image_sizes[idx])
         result_img.paste(img, (0, y_offset))
-        y_offset += img.size[1]
+        y_offset += (img.size[1] + img_space) # height 값 + 사용자가 지정한 간격
 
         progress = (idx + 1)/len(images) * 100 # 실제 퍼센트 계산
         p_var.set(progress)
         progress_bar.update()
 
-    dest_path = os.path.join(txt_dest_path.get(), "testphoto.jpg")
+
+    #포맷 옵션 처리
+    file_name = "testphoto." + img_format
+    dest_path = os.path.join(txt_dest_path.get(), file_name)
     result_img.save(dest_path)
     messagebox.showinfo("알림", "작업이 완료되었습니다.")
 #시작
