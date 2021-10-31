2f1b57330598892da7627df20241c2a0c4f12f26
namjiseong <skawltjd2000@naver.com>
2021-10-31 21:25:10 +0900

    종료 버튼 추가, 영상 좌우반전, 화면 키보드 메소드 추가 약 16분까지 내용

--- python/머신러닝/가상키보드/1031jiseongtest.py
+++ python/머신러닝/가상키보드/1031jiseongtest.py
@@ -5,14 +5,35 @@ cap = cv2.VideoCapture(0)
 cap.set(3,1280)
 cap.set(4,720)
 
-detector = HandDetector()
+detector = HandDetector(detectionCon=0.8)
+
+class Button():
+    def __init__(self, pos, text, size=[100,100]):
+        self.pos = pos
+        self.size = size
+        self.text = text
+        cv2.rectangle(img, self.pos, self.size, (255, 0,255), cv2.FILLED)
+        cv2.putText(img, self.text, (self.pos[0]+25, self.pos[1]+25), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+
+
+
+
 
 while True:
     
     success, img = cap.read()
+    img = cv2.flip(img, 1) #화면 좌우 반전
+
+    
     img = detector.findHands(img)
     lmlist, bboxInfo = detector.findPosition(img)
     
-    
+    mybutton = Button([100,100], "Q")
+
     cv2.imshow("image", img)
-    cv2.waitKey(1)
+    exit = cv2.waitKey(1)
+
+
+    #종료버튼 : q
+    if exit ==ord("q"): 
+        break
