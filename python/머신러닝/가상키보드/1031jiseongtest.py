7f31cbf6078ad5e191ff9302452f834b561e8a7f
namjiseong <skawltjd2000@naver.com>
2021-11-01 19:09:00 +0900

    35분까지 -키보드 이미지 구현

--- python/머신러닝/가상키보드/1031jiseongtest.py
+++ python/머신러닝/가상키보드/1031jiseongtest.py
@@ -7,28 +7,48 @@ cap.set(4,720)
 
 detector = HandDetector(detectionCon=0.8)
 
+keys = [['Q', 'W', 'E','R','T','Y','U','I','O','P','[',']'],
+       ['A','S','D','F','G','H','J','K','L',';',"'"],
+       ['Z','X','C','V','B','N','M',',','.','/'] ]
+def drawALL(img, buttonList):
+    for button in buttonList:
+
+        x, y = button.pos
+        w, h = button.size
+        cv2.rectangle(img, button.pos, (x+w, y+h), (255, 0,255), cv2.FILLED)
+        cv2.putText(img, button.text, (x+20, y+65), cv2.FONT_HERSHEY_PLAIN,
+        4, (255, 255, 255), 4)
+
+    return img
+
 class Button():
-    def __init__(self, pos, text, size=[100,100]):
+    def __init__(self, pos, text, size=[85,85]):
         self.pos = pos
         self.size = size
         self.text = text
-        cv2.rectangle(img, self.pos, self.size, (255, 0,255), cv2.FILLED)
-        cv2.putText(img, self.text, (self.pos[0]+25, self.pos[1]+25), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
 
 
+        
 
 
+buttonList = []
+for i in range(len(keys)):
+        for j, key in enumerate(keys[i]):
+            buttonList.append(Button([100*j+50, 100*(i+1)+50], key))
 
+
+    
 while True:
     
     success, img = cap.read()
     img = cv2.flip(img, 1) #화면 좌우 반전
 
-    
     img = detector.findHands(img)
     lmlist, bboxInfo = detector.findPosition(img)
     
-    mybutton = Button([100,100], "Q")
+    img = drawALL(img, buttonList)
+    
+
 
     cv2.imshow("image", img)
     exit = cv2.waitKey(1)
